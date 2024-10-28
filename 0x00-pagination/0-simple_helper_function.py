9663521e935f8ba14db08d79ab842b2e2a2fa5bd
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-28 19:23:55 +0000

    whitespace

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+"""
+Defines a function named `index_range`
+"""
+from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple containing the start and end index
     for the items on a specific page.
