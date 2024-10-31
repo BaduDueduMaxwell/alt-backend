bc9da8bbd43b70f7548d09de2e92308b9c02a1ce
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-31 01:03:03 +0000

    task 2:LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -17,7 +17,7 @@ class LIFOCache(BaseCaching):
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 last_key = next(reversed(self.cache_data))
-                self.cache_data.pop(last_key)
+                del self.cache_data[last_key]
                 print(f"DISCARD: {last_key}")
 
             self.cache_data[key] = item
