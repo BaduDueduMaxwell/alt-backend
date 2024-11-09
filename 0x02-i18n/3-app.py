75473d32ac32cac6d55b2e754df4b80b71078a07
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-09 13:53:13 +0000

    pycodestyle fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
 from flask import Flask, render_template
 from flask_babel import Babel, _
+
 
 app = Flask(__name__)
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 babel = Babel(app)
 
+
 @app.route('/')
 def home():
+    """Route to render 3-index.html"""
     return render_template('3-index.html')
+
 
 if __name__ == "__main__":
     app.run()
