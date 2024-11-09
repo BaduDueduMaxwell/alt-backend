2301877841193e097e68aad54b64c0e04539b79d
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-11-09 13:54:54 +0000

    pycodestyle fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,20 +2,34 @@
 """
 A Basic flask application
 """
-from flask import Flask, render_template
-from flask_babel import Babel, _
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
+class Config:
+    """Config class to configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate Flask app, set config, and initialize Babel
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """Function to determine the best match with supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
-def home():
-    """Route to render 3-index.html"""
+def index():
+    """Route to render the index page"""
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
